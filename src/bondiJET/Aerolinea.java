4e68006b6ab996923bdbe16b071a5bfcba208dfe
Matias <Matias@192.168.0.19>
2024-11-02 14:22:42 -0300

    first commit

---
+++ src/bondiJET/Aerolinea.java
@@ -0,0 +1,10 @@
+package bondiJET;
+
+public class Aerolinea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/bondiJET/Asiento.java
@@ -0,0 +1,6 @@
+package bondiJET;
+
+public class Asiento {
+
+
+}
