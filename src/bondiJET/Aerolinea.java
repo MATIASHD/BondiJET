6fb44fde9d81a3b84d7b14476a4446bd6aa65c61
Facundo Sosa <js1697436@gmail.com>
2024-11-03 14:51:10 -0300

    interface agregada

--- src/bondiJET/Aerolinea.java
+++ src/bondiJET/Aerolinea.java
@@ -1,10 +1,87 @@
 package bondiJET;
 
-public class Aerolinea {
+import java.util.*;
+import java.util.Map;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Aerolinea implements IAerolinea {
+	
 
+	public Aerolinea(String nombre, String CUIT) {
+		
+		this.nombre = nombre;
+		this.cuit = CUIT;
+		
 	}
+
+    String nombre;
+    String cuit;
+    LinkedList<Aeropuerto> aeropuertos;
+    LinkedList<Vuelo> vuelos;
+    LinkedList<Cliente> clientes;
+
+    public void registrarCliente(int dni, String nombre, String telefono) {
+
+    }
+
+    public void registrarAeropuerto(String nombre, String pais, String provincia, String direccion) {
+    
+    }
+
+    public String registrarVueloPublicoNacional(String origen, String destino, String fecha, int tripulantes,
+            double valorRefrigerio, double[] precios, int[] cantAsientos) {
+
+        return "";
+    }
+
+    public String registrarVueloPublicoInternacional(String origen, String destino, String fecha, int tripulantes,
+            double valorRefrigerio, int cantRefrigerios, double[] precios, int[] cantAsientos, String[] escalas) {
+                
+        return "";
+    }
+
+    public String VenderVueloPrivado(String origen, String destino, String fecha, int tripulantes, double precio,
+            int dniComprador, int[] acompaniantes) {
+    
+        return "";
+    }
+
+    public Map<Integer, String> asientosDisponibles(String codVuelo) {
+
+        return null;
+    }
+
+    public int venderPasaje(int dni, String codVuelo, int nroAsiento, boolean aOcupar) {
+
+        return 0;
+    }
+
+    public List<String> consultarVuelosSimilares(String origen, String destino, String Fecha) {
+
+        return null;
+    }
+
+    public void cancelarPasaje(int dni, String codVuelo, int nroAsiento) {
+
+    }
+
+    public void cancelarPasaje(int dni, int codPasaje) {
+
+    }
+
+    public List<String> cancelarVuelo(String codVuelo) {
+
+        return null;
+    }
+
+    public double totalRecaudado(String destino) {
+
+        return 0;
+    }
+
+    public String detalleDeVuelo(String codVuelo) {
+
+        return "";
+    }
+
 
 }
